3ee3f241f68340508fecaadac54ebd053629763f
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-27 11:54:42 +0200

    Update Song.java
    class added

--- MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/Song.java
+++ MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/Song.java
@@ -1,1 +1,64 @@
+package be.pxl.musicplaylist.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Song {
+    private String title;
+    private String artist;
+    @JsonProperty("duration_seconds")
+    private int durationSeconds;
+    private Genre genre;
+
+    public Song() {
+
+    }
+
+    public Song(String title, String artist, int durationSeconds, Genre genre) {
+        this.title = title;
+        this.artist = artist;
+        this.durationSeconds = durationSeconds;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+
+    public void setDurationSeconds(int durationSeconds) {
+        this.durationSeconds = durationSeconds;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", durationSeconds=" + durationSeconds +
+                ", genre=" + genre +
+                '}';
+    }
+}
