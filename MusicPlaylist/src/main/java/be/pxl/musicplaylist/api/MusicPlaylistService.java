46d7eac6fc75eb610e0fa6a162b85b6a6c695b8e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-01 12:21:02 +0200

    Update MusicPlaylistService.java
    start code added

--- MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/MusicPlaylistService.java
+++ MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/MusicPlaylistService.java
@@ -1,1 +1,11 @@
+package be.pxl.musicplaylist.api;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MusicPlaylistService {
+  
+}
