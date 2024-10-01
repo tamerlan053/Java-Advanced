a079fe991ca3dab632b988da74cbeadef18e7749
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-01 12:22:21 +0200

    Update MusicPlaylistService.java
    Playlist Service finished

--- MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/MusicPlaylistService.java
+++ MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/MusicPlaylistService.java
@@ -7,5 +7,22 @@ import java.util.List;
 
 @Service
 public class MusicPlaylistService {
-  
+      private final List<Song> myPlaylist = new ArrayList<>();
+    public void addSong(Song song) {
+        myPlaylist.add(song);
+    }
+
+    public List<Song> getAllSongs() {
+        return myPlaylist;
+    }
+
+    public List<Song> getAllSongsByGenre(Genre genre) {
+        List<Song> songsByGenre = new ArrayList<>();
+        for (Song song: myPlaylist) {
+            if (song.getGenre() == genre) {
+                songsByGenre.add(song);
+            }
+        }
+        return songsByGenre;
+    }
 }
