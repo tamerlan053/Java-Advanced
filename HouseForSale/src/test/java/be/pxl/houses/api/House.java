bd644b4c05730f20a25d49d662c377c7c92066fd
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-03 17:33:13 +0200

    Update House.java
    getters and setter added

--- HouseForSale/src/test/java/be/pxl/houses/api/House.java
+++ HouseForSale/src/test/java/be/pxl/houses/api/House.java
@@ -6,4 +6,44 @@ public class House {
     private HouseStatus status;
     private String city;
     private double price;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HouseStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HouseStatus status) {
+        this.status = status;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
