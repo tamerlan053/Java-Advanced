4b7e29988365ab91297c453167959c4c892adc52
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-03 17:32:44 +0200

    Update House.java
    instance variables added

--- HouseForSale/src/test/java/be/pxl/houses/api/House.java
+++ HouseForSale/src/test/java/be/pxl/houses/api/House.java
@@ -1,1 +1,9 @@
+package be.pxl.houses.api;
 
+public class House {
+    private String code;
+    private String name;
+    private HouseStatus status;
+    private String city;
+    private double price;
+}
