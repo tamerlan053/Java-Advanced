c016b958c7e0a738e49238ac1b33f8e200ae695e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-03 17:30:43 +0200

    start position added

---
+++ HouseForSale/src/main/java/be/pxl/houses/HouseForSaleApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.houses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseForSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseForSaleApplication.class, args);
+	}
+
+}
