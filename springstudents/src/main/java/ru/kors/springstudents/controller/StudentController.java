63e38b8fef7e99b88abb725a7c78624a42cc5f90
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-06 13:03:31 +0200

    Create StudentController.java
    StudentController class created

---
+++ springstudents/src/main/java/ru/kors/springstudents/controller/StudentController.java
@@ -0,0 +1,10 @@
+package ru.kors.springstudents.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+}
