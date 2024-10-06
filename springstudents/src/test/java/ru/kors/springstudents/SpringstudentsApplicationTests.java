cbd84104a6cb6367c7247532359a85eaee98519a
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-06 12:39:27 +0200

    Add files via upload

---
+++ springstudents/src/test/java/ru/kors/springstudents/SpringstudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kors.springstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
