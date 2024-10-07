a45104fba3d0167b21f4a2530c37b7ce7b1caa51
tamerlan <tamerlan.aslakhanov@gmail.com>
2024-10-07 20:44:24 +0200

    service interface & repository

--- springstudents/src/main/java/ru/kors/springstudents/service/Impl/InMemoryStudentServiceImpl.java
+++ springstudents/src/main/java/ru/kors/springstudents/service/Impl/InMemoryStudentServiceImpl.java
@@ -1,19 +1,39 @@
 package ru.kors.springstudents.service.Impl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.kors.springstudents.model.Student;
+import ru.kors.springstudents.repository.InMemoryStudentDao;
 import ru.kors.springstudents.service.StudentService;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class InMemoryStudentServiceImpl implements StudentService {
+    private final InMemoryStudentDao repository;
     @Override
     public List<Student> findAllStudents() {
-        return List.of(
-                Student.builder().firstName("John").email("john19@gmail.com").age(37).build(),
-                Student.builder().firstName("Jessa").email("jessa57@gmail.com").age(53).build(),
-                Student.builder().firstName("Marco").email("marco101@gmail.com").age(21).build()
-        );
+        return repository.findAllStudents();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return repository.saveStudent(student);
+    }
+
+    @Override
+    public Student findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return repository.updateStudent(student);
+    }
+
+    @Override
+    public void deleteStudent(String email) {
+        repository.deleteStudent(email);
     }
 }
--- springstudents/src/main/java/ru/kors/springstudents/service/StudentService.java
+++ springstudents/src/main/java/ru/kors/springstudents/service/StudentService.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface StudentService {
     List<Student> findAllStudents();
+    Student saveStudent(Student student);
+    Student findByEmail(String email);
+    Student updateStudent(Student student);
+    void deleteStudent(String email);
 }
