31f66ab2c46ce81dd09b743efb327d8bfc741295
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-11 22:58:22 +0200

    MessageDigestService implemented

--- MusicPlaylist/src/main/java/be/pxl/musicplaylist/service/MessageDigestService.java
+++ MusicPlaylist/src/main/java/be/pxl/musicplaylist/service/MessageDigestService.java
@@ -7,4 +7,15 @@ import java.security.NoSuchAlgorithmException;
 
 @Service
 public class MessageDigestService {
+    public String calculateHash(String algorithm, String original) throws NoSuchAlgorithmException {
+        MessageDigest md;
+        md = MessageDigest.getInstance(algorithm);
+        md.update(original.getBytes());
+        byte[] digest = md.digest();
+        StringBuilder result = new StringBuilder();
+        for (byte b : digest) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
 }
