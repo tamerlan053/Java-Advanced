dea6d57b68bf90e79bbb4a6b1802ecbe2bb3d193
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-11 22:58:04 +0200

    service class template added

--- MusicPlaylist/src/main/java/be/pxl/musicplaylist/service/MessageDigestService.java
+++ MusicPlaylist/src/main/java/be/pxl/musicplaylist/service/MessageDigestService.java
@@ -1,1 +1,10 @@
+package be.pxl.musicplaylist.service;
 
+import org.springframework.stereotype.Service;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+public class MessageDigestService {
+}
