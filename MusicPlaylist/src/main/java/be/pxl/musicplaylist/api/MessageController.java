2f59d9bc94d4d0c5d2f77cd685ce380fbc09a29b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-10-12 18:11:44 +0200

    Startcode added

--- MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/MessageController.java
+++ MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/MessageController.java
@@ -1,1 +1,16 @@
+package be.pxl.musicplaylist.api;
 
+import be.pxl.musicplaylist.service.MessageDigestService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+@RequestMapping("/hash")
+public class MessageController {
+  
+}
