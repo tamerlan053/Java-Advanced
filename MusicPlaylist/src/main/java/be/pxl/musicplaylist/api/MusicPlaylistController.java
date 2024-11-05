e7a09633d301fa1e8a28a62f2f378121266eb89e
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-05 13:47:30 +0100

    Use of MusicPlaylistService in the Controller

--- MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/MusicPlaylistController.java
+++ MusicPlaylist/src/main/java/be/pxl/musicplaylist/api/MusicPlaylistController.java
@@ -1,17 +1,25 @@
 package be.pxl.musicplaylist.api;
 
 import org.apache.logging.log4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(".playlist/songs")
+@RequestMapping("playlist/songs")
 public class MusicPlaylistController {
     private static final Logger LOGGER = LogManager.getLogger(MusicPlaylistController.class);
+    private final MusicPlaylistService musicPlaylistService;
+
+    @Autowired
+    public MusicPlaylistController(MusicPlaylistService playlistService) {
+        this.musicPlaylistService = playlistService;
+    }
 
     @PostMapping
     public void addSong(@RequestBody Song song) {
         if (LOGGER.isInfoEnabled()) {
             LOGGER.info("Adding song [" + song.getTitle() + "]");
         }
+        musicPlaylistService.addSong(song);
     }
 }
