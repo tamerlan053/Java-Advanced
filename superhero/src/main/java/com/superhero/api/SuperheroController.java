c8fd04ebd1bb1f82ffa14196e4c17574abca0ac1
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-24 23:29:59 +0100

    SuperheroService DI

--- superhero/src/main/java/com/superhero/api/SuperheroController.java
+++ superhero/src/main/java/com/superhero/api/SuperheroController.java
@@ -1,4 +1,14 @@
 package com.superhero.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/superheroes")
 public class SuperheroController {
+    private final SuperheroService superheroService;
+
+    public SuperheroController(SuperheroService superheroService) {
+        this.superheroService = superheroService;
+    }
 }
