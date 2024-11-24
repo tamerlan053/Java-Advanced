71dd9e47d2a580ba728a3d4aa8df01de52768d59
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-24 23:31:50 +0100

    SuperheroService constructor

--- superhero/src/main/java/com/superhero/service/SuperheroService.java
+++ superhero/src/main/java/com/superhero/service/SuperheroService.java
@@ -1,4 +1,12 @@
 package com.superhero.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SuperheroService {
+    private final SuperheroRepository superheroRepository;
+
+    public SuperheroService(SuperheroRepository superheroRepository) {
+        this.superheroRepository = superheroRepository;
+    }
 }
