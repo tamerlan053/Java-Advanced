28f95c5ec7936ad256ec05fa1b824782d64c18cc
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-28 19:55:50 +0100

    HouseService fully implemented

--- HouseForSale/src/main/java/be/pxl/houses/service/HouseService.java
+++ HouseForSale/src/main/java/be/pxl/houses/service/HouseService.java
@@ -1,4 +1,38 @@
 package be.pxl.houses.service;
 
+import be.pxl.houses.api.House;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class HouseService {
+
+    private final Map<String, House> houses = new HashMap<>();
+
+    public void addHouse(House house) {
+        houses.put(house.getCode(), house);
+    }
+
+    public List<House> getHouses() {
+        return new ArrayList<>(houses.values());
+    }
+
+    public void updateHouse (String code, House data) {
+        if (houses.containsKey(code)) {
+            data.setCode(code);
+            houses.put(code, data);
+        }
+    }
+
+    private House findHouseByCode (String code){
+        return houses.get(code);
+    }
+
+    public void deleteHouse (String code){
+        houses.remove(code);
+    }
 }
