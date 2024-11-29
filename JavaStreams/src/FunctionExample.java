ee52c9b08f9c54f3b3b3b9ed96f748d22fa745ab
Tamerlan <tamerlan.aslakhanov@gmail.com>
2024-11-29 22:36:14 +0100

    Java Streams using java.util.function.Function

---
+++ JavaStreams/src/FunctionExample.java
@@ -0,0 +1,14 @@
+import java.util.function.Function;
+
+public class FunctionExample {
+    public static void main(String[] args) {
+        // Define a Function that converts a String to its length (anInteger)
+        Function<String, Integer> stringLengthFunction = str -> str.length();
+
+        // Apply the function to a String
+        String inputString = "Hello, Function!";
+        int length = stringLengthFunction.apply(inputString);
+
+        System.out.println("Length of the string: " + length);
+    }
+}
